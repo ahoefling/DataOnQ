a6b166fed1ae69a43da2865416c63b1072d6fe1d
Andrew Hoefling <andrewh@fileonq.com>
2020-01-18 13:52:56 -0500

    Added Xamarin.Forms sample project

---
+++ Samples/Xamarin.Forms/DataOnQ.Samples.Xamarin.Forms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DataOnQ.Samples.Xamarin.Forms.Models;
+
+namespace DataOnQ.Samples.Xamarin.Forms.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
